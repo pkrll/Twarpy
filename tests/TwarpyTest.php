7996e0df412b87afbaeff6edc3854881b71a111f
Ardalan Samimi <ardalan.samimi@gmail.com>
2015-08-13 22:25:28 +0200

    Add unit test

---
+++ tests/TwarpyTest.php
@@ -0,0 +1,22 @@
+<?php
+use Twarpy\Twarpy;
+
+class TwarpyTest extends \PHPUnit_Framework_TestCase {
+
+    public function testAuthAndTimeline() {
+        $config = array(
+            'consumerKey'       => getenv('consumerKey'),
+            'consumerSecret'    => getenv('consumerSecret')
+        );
+
+        $Twarpy = new Twarpy($config);
+        $this->assertInternalType('array', $Twarpy->getOAuthToken());
+
+        $params = array("screen_name" => "twitter");
+        $data = $Twitter->request('statuses/user_timeline', 'GET', $params);
+        $this->assertInternalType('array', $data);
+    }
+
+}
+
+?>
